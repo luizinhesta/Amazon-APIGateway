17937a2e5f3873e8fe0b7c06f547b048812b1f98
Luiz <luizaugusouza@gmail.com>
2026-04-19 18:54:36 -0300

    atullização

--- Nova pasta/app-config.js
+++
@@ -1,4 +0,0 @@
-﻿window.APP_CONFIG = {
-  API_BASE_URL: 'https://SEU-ALB-OU-APIGW-DA-API',
-  CEP_API_URL: 'https://SEU-APIGW-CEP/cep'
-};
--- Nova pasta/ecs/config/app.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'base_url' => '',
-    'session_name' => 'lab_session',
-];
